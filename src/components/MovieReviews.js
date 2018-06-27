5e0568b95de85a42c9c8829f505491edd90ca621
blevm <mjblevins7@gmail.com>
2018-06-27 19:22:51 -0400

    wip

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => {
+  MovieReviews.defaultProps = {reviews: props.reviews}
+
+  const MovieList = props.reviews.map(review =>  (<div className="review">{review.display_name}</div>))
+
+  return (
+    <div className="review-list">
+      {MovieList}
+    </div>
+  )
+}
+
+
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,31 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react';
+
+class SearchableMovieReviews extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  render() {
+    console.log(this.state.searchTerm)
+    return(
+      <div className="searchable-movie-reviews">
+      <input type="text" placeholder="Search" onChange={this.handleChange}/>
+
+      </div>
+    )
+  }
+
+}
+
+export default SearchableMovieReviews;
