777e9f0ffcba1bbd5c9676cd9f6640be10cd3e22
blevm <mjblevins7@gmail.com>
2018-06-28 22:02:29 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieReviews = (props) => {
   MovieReviews.defaultProps = {reviews: props.reviews}
 
-  const MovieList = props.reviews.map(review =>  (<div className="review">{review.display_name}</div>))
+  const MovieList = props.reviews.map(review =>  (<div className="review"><a href={review.link.url} target="_blank">{review.headline}</a></div>))
 
   return (
     <div className="review-list">
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import MovieReviews from './MovieReviews'
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query='
+const api_key_string = `&api-key=${NYT_API_KEY}`;
+
 
 class SearchableMovieReviews extends React.Component {
   constructor() {
@@ -16,12 +22,21 @@ class SearchableMovieReviews extends React.Component {
     })
   }
 
+  performSearch = (event) => {
+    event.preventDefault()
+    console.log(event)
+
+    fetch(`${BASE_URL}${this.state.searchTerm}${api_key_string}`)
+      .then(response => response.json())
+      .then(data => this.setState({ reviews: data.results }));
+  }
+
   render() {
-    console.log(this.state.searchTerm)
     return(
       <div className="searchable-movie-reviews">
-      <input type="text" placeholder="Search" onChange={this.handleChange}/>
-
+        <input type="text" placeholder="Search" onChange={this.handleChange}/>
+        <button type="submit" onClick={this.performSearch}>Submit</button>
+        <MovieReviews reviews={this.state.reviews} />
       </div>
     )
   }
